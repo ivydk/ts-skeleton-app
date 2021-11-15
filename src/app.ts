a922a5e08d9614d0b254e7842fdee42c7a330b23
ivydk <dekk0123@hz.nl>
2021-11-15 11:05:35 +0100

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,46 @@
-class Animal
-{
+class Animal {
   private readonly name: string;
 
   private readonly legs: number;
 
   private readonly sound: string;
 
+  /**
+   * geeeen idee
+   *
+   * @param name the name
+   * @param legs the number of legs
+   * @param sound the sound
+   */
   public constructor(name: string, legs: number, sound: string) {
     this.name = name;
     this.legs = legs;
     this.sound = sound;
   }
 
+  /**
+   * gezelligheid
+   *
+   * @returns the name
+   */
   public getName(): string {
     return this.name;
   }
 
+  /**
+   * uitleg ..
+   *
+   * @returns number of legs
+   */
   public getLegs(): number {
     return this.legs;
   }
 
+  /**
+   * uitleg
+   *
+   * @returns what sound it makes
+   */
   public getSound(): string {
     return this.sound;
   }
